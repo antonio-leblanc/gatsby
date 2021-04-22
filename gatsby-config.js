3348daed61622af204b65c06896657497029f3d0
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-22 00:22:15 -0300

    Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,11 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+// module.exports = {
+//   /* Your site config here */
+//   plugins: [],
+// }
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  pathPrefix: "/gatsby",
 }
