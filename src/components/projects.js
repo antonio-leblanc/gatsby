0cbd4d3cdac24493c7d32254d520bb247049347c
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 19:37:42 -0300

    material-ui

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import Button from '@material-ui/core/Button';
 
 export default function Projects() {
 
@@ -10,6 +11,9 @@ export default function Projects() {
       <div>Pantera</div>
       <div>ThreeJs</div>
       <div>FireMap</div>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     </div>
     </>
   )
