a12f61c95600b1d2881629deb6ece448737f5426
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 19:55:43 -0300

    uicons

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save'
+
+
 
 export default function Projects() {
 
-
-  
   return (<>
     <div data-aos="fade-left">
       <div>Pantera</div>
       <div>ThreeJs</div>
       <div>FireMap</div>
-      <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+      <Button
+      endIcon={<SaveIcon/>}
+      onClick={()=>console.log('hey')}
+      variant="contained"
+      color="primary">
+        Hello World
+      </Button>
     </div>
     </>
   )
