6c1a81b5bc1ef2d93860d49e4c4f4fbf3f20f96f
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 18:39:25 -0300

    EDUCATION

--- src/components/education.js
+++ src/components/education.js
@@ -3,11 +3,52 @@ import React from 'react';
 export default function Education() {
 
   return (<>
-    <div data-aos="fade-left">
-      <div>UFJR</div>
-      <div>CENTRALE</div>
-      <div>VAMO</div>
-    </div>
+    <div class="subsection-content"><h1 class="content-title">
+        Education</h1>
+      <div class = "content-block">
+        <h3 class ="block-title">
+          Ecole Centrale de Lyon
+        </h3>
+        <div class="styled-list">
+        <ul>
+          <li> General Engineering</li>
+          <li> Coach of the Handball</li>
+          <li> Member of the
+            <a target= '_blank' href="http://www.epsa-team.com/index.php/en/competitions/formula-student">
+            Formula Student Team
+            </a>
+          </li>
+          <li>
+            Interview for the
+            <a target= '_blank' href="https://www.centraliens-lyon.net/technica/article/centralien-de-l-etranger-antonio-leblanc-de-l-ecl-a-une-start-up-bresilienne-specialisee-dans-la-detection-d-incendies-de-forets/217">
+              School Magazine
+            </a>
+          </li>
+        </ul>
+        </div>
+      </div>
+      
+      <div class = "content-block">
+        <h3 class ="block-title">Universidade Federal do Rio de Janeiro</h3>
+        <div class="styled-list">
+        <ul>
+          <li>Mechanical Engineering</li>
+          <li>
+            Member of the 
+            <a target= "_blank" href= "http://minervabots.poli.ufrj.br/">
+            Robotics Team
+            </a>
+          </li>
+          <li>
+            <a target= "_blank" href= "http://www.monografias.poli.ufrj.br/monografias/monopoli10031238.pdf">
+              Final Project
+            </a>
+          </li>
+          
+        </ul>
+        </div>
+      </div>
+      </div>
     </>
   )
 }
