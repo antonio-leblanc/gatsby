802a624fd7ee487a847d62bd26963fe74c4ee1dd
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 20:16:45 -0300

    maaaap

--- src/components/map.js
+++ src/components/map.js
@@ -1,9 +1,23 @@
 import React from 'react';
+// import React, { useEffect } from 'react';
+ 
+// import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Map() {
+  // const defaultPosition = [48.864716, 2.349]; // Paris position
 
-  return (<>
-MAP
-    </>
-  )
-}
+  return (
+    <h1>MAP</h1>
+    // <div className="map__container">
+    //   <MapContainer
+    //     center={defaultPosition}
+    //     zoom={13}
+    //   >
+    //     <TileLayer
+    //       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     />
+    //   </MapContainer>
+    // </div>
+  );
+};
