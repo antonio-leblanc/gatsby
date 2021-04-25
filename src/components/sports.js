60f77fdfb44549815cb94b225c7d153dd77e8664
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 21:40:44 -0300

    sports

--- src/components/sports.js
+++ src/components/sports.js
@@ -2,9 +2,41 @@ import React from 'react';
 
 export default function Sports() {
 
-  return (
-<section id= "music" className='music section'>
-Sports
+  return ( <>
+<section className="bg-img-3 section"></section>
+
+<section id= "sports" className='sports section'>
+  <h1 className="text-mask" data-aos="fade-up">
+    SPORTS
+  </h1>
+  
+  <section className="subsection tri-subsection">
+    <div className="img-container">
+    <img className="rounded-img" src={'/images/mft.jpeg'} alt="Logo" />
+
+    </div>
+    <div className="subsection-content">
+      <div className="content-block">
+        <h3 className ="block-title">
+          {/* <img className="header-icon" src="images/icons/bike.svg"> */}
+          Triathlon  
+        </h3>
+        <div className="styled-list">
+          <ul>
+            <li>Member of <a href="https://www.mftriathlon.com/" target ="_blank">Marcia Ferreira's</a> 
+              Triathlon Team</li>
+            <li>Rio Duathlon 2020 - 2nd Place [20-24 years]</li>
+            <li>Cylcing</li>
+            <li>Running</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className="img-container">
+    <img className="rounded-img" src={'/images/tri4.jpeg'} alt="Logo" />
+    </div>
   </section>
+  </section>
+  </>
   );
 };
