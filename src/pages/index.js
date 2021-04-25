4b5c18c7b9b6e431a4304a8cbe1f12b5bfb77c3b
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 23:03:09 -0300

    poiret

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import Sports from "../components/sports"
 import AOS from 'aos';
 import "../styles/layout.css"
 import "@fontsource/montserrat" // Defaults to weight 400.
+import "@fontsource/poiret-one" // Defaults to weight 400.
 
 export default function Home() {
   useEffect(() => {
