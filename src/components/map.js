5c62055c34b3bed6e1595da65757554acb0be7e5
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 21:54:34 -0300

    trying map

--- src/components/map.js
+++ src/components/map.js
@@ -1,23 +1,22 @@
 import React from 'react';
 // import React, { useEffect } from 'react';
- 
 // import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Map() {
   // const defaultPosition = [48.864716, 2.349]; // Paris position
 
-  return (
-    <h1>MAP</h1>
-    // <div className="map__container">
-    //   <MapContainer
-    //     center={defaultPosition}
-    //     zoom={13}
-    //   >
-    //     <TileLayer
-    //       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    //     />
-    //   </MapContainer>
-    // </div>
+  return (<>
+  <section class="bg-img-4 section"></section>
+
+  <section class="travel section" id= "travel">
+    <h1 class="text-mask" data-aos="fade-up">
+      Travel
+    </h1> 
+    
+    <div>
+      <div id="travel-map"></div>
+    </div>
+  </section>
+  </>
   );
 };
